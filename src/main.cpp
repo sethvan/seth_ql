56c7aa9c05a4cd02c64047d03e832c7f2bc9609d
Sethvan <sethvan@yahoo.com>
2023-01-23 10:46:52 -0700

    Last commit before change to polymorphism

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+/*
+   Third commit note: Unfinished idea, abandoning as I believe I could make a better more
+   practical/extensible version using polymorphism now that I have acquainted myself more
+   with my options. Committing this as it for future reference and will immediately change
+   to a polymorphic version after commit.
+
+*/
+
+#include <algorithm>
+#include <concepts>
+#include <initializer_list>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <string_view>
 #include <tuple>
+#include <type_traits>
 #include <vector>
 
 #include "fieldTypes.hpp"
@@ -21,7 +35,11 @@ const auto fieldTypes = getFieldTypesArray();
    May decide to make separate classes for rows that are meant to read or write.
    Need to also consider case where only certain fields are updated.
 */
+
+enum class RowField { TINY, SMALL, BIG, FIRST_NAME };
+
 class IntsTableRow {
+
    private:
     std::tuple<TinyInt, SmallInt, BigInt, TextType> fields;
 
@@ -30,7 +48,7 @@ class IntsTableRow {
     SmallInt* small;
     BigInt* big;
     TextType* firstName;
-    MYSQL_BIND binds[4];
+    std::vector<MYSQL_BIND> binds;
     IntsTableRow()
         : fields( std::make_tuple( TinyInt( "tiny" ), SmallInt( "small" ),
                                    BigInt( "big" ), TextType( "first_name" ) ) ),
@@ -38,24 +56,6 @@ class IntsTableRow {
           small( &std::get<1>( fields ) ),
           big( &std::get<2>( fields ) ),
           firstName( &std::get<3>( fields ) ) {
-        std::memset( binds, 0, sizeof( binds ) );
-        std::apply(
-            [&]( auto&... tupleArgs ) {
-                int i = 0;
-                auto bindFieldsToBindsArray = [&]( auto& o ) {
-                    o.bind = &binds[i++];
-                    o.bind->buffer_type = o.bufferType;
-                    o.bind->buffer = (char*)o.buffer;
-                    o.bind->is_null = &o.isNull;
-                    o.bind->length = &o.length;
-                    o.bind->error = &o.error;
-                    if ( o.bufferType == MYSQL_TYPE_STRING ) {
-                        o.bind->buffer_length = o.bufferLength;
-                    }
-                };
-                ( bindFieldsToBindsArray( tupleArgs ), ... );
-            },
-            fields );
     }
     void displayFields() {
         std::apply(
@@ -75,23 +75,80 @@ class IntsTableRow {
             },
             fields );
     }
+
+    // template <typename T>
+    // void setFieldValue( const char* name, const T& t ) {
+
+    //     auto setIt = [&]( auto&... object ) {
+    //         ( ( object.fieldName == name && ( object.setValue( t ), 1 ) ) || ... );
+    //     };
+
+    //     std::apply( setIt, fields );
+    // }
+
+    template <typename... Ts>
+    using AllRfs = typename std::conjunction<std::is_same<Ts, RowField>...>::type;
+
+    template <typename... Ts>
+
+    void selectFields( Ts... ts ) {
+        auto bindFieldsToBindsArray = [&]( auto* o ) {
+            o->bind = &( *std::next( binds.end(), -1 ) );
+            std::memset( &( *o->bind ), 0, sizeof( *o->bind ) );
+            o->bind->buffer_type = o->bufferType;
+            o->bind->buffer = (char*)o->buffer;
+            o->bind->is_null = &o->isNull;
+            o->bind->length = &o->length;
+            o->bind->error = &o->error;
+            if ( o->bufferType == MYSQL_TYPE_STRING ) {
+                o->bind->buffer_length = o->bufferLength;
+            }
+        };
+
+        // does not maintain correct sequence, idea unfinished
+        const auto makeSelection = [&]( RowField& selection ) {
+            binds.emplace_back();
+            switch ( selection ) {
+                case RowField::TINY:
+                    bindFieldsToBindsArray( tiny );
+                    break;
+                case RowField::SMALL:
+                    bindFieldsToBindsArray( small );
+                    break;
+                case RowField::BIG:
+                    bindFieldsToBindsArray( big );
+                    break;
+                case RowField::FIRST_NAME:
+                    bindFieldsToBindsArray( firstName );
+                    break;
+                default:
+                    /*Code to select all here*/
+                    break;
+            }
+        };
+        ( makeSelection( ts ), ... );
+    }
 };
 
 int main() {
 
     IntsTableRow row;
     row.tiny->setValue( 90 );
-    row.big->setValue( 89 );
-    row.small->setValue( 100 );
+    row.big->setValue( 8912345 );
+    row.small->setValue( 3 );
     row.firstName->setValue( "John" );
 
     puts( "" );
-    for ( int i = 0; i < 4; ++i ) {
-        std::cout << "row.binds[" << i
-                  << "].buffer_type:   " << fieldTypes[row.binds[i].buffer_type] << '\n';
-    }
 
     row.displayFields();
+
+    row.selectFields( RowField::TINY, RowField::SMALL, RowField::BIG );
+
+    int i = 0;
+    std::for_each( row.binds.begin(), row.binds.end(), [&]( const auto& bind ) {
+        std::cout << "row.binds[" << i++
+                  << "].buffer_type:   " << fieldTypes[bind.buffer_type] << '\n';
+    } );
 
     puts( "" );
 
