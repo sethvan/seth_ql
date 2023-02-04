333b42e32535dfbcd16b099f2e5ba41dda9ad884
Sethvan <sethvan@yahoo.com>
2023-02-03 19:21:23 -0700

    Made helper functions static

--- include/createDBTableBinds.h
+++ include/createDBTableBinds.h
@@ -17,22 +17,6 @@ namespace binds_wrapper {
                              unsigned long buff_size );  // buff_size will be the buff size for all the
                                                          // binds created that are char[]
 
-    void replaceWord( std::string& str, const std::string& pattern, const std::string& replacement );
-
-    void setDeclHeaderAndFooter( std::ostringstream& declaration_header, std::ostringstream& declaration_footer,
-                                 std::string db_name );
-
-    std::ostringstream createDefinitionHeader( const std::string& includeStr );
-
-    void setFileBodies( std::ostringstream& declaration_body, std::ostringstream& definition_body,
-                        std::span<const Table> tables, unsigned long buff_size );
-
-    void writeDeclarationFile( const std::ostringstream& declaration_header, const std::ostringstream& declaration_body,
-                               const std::ostringstream& declaration_footer, const std::string& fileName );
-
-    void writeDefinitionFile( const std::ostringstream& definition_header, const std::ostringstream& definition_body,
-                              const std::string& fileName );
-
 }  // namespace binds_wrapper
 
 #endif  // INCLUDED_CREATEDBTABLEBINDS_H
