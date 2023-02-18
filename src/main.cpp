c316206073c63210034b1969d4aefa7e73c380ab
Sethvan <sethvan@yahoo.com>
2023-02-18 11:12:48 -0700

    Updated Input type in library, now can set value members of different types in polymorphic array without casting

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
 #include <mysql/mysql.h>
 
-#include <cstring>
-#include <initializer_list>
 #include <iostream>
 
-#include "myvars.h"
 #include "set_mysql_binds.h"
 
 using namespace set_mysql_binds;
+using enum set_mysql_binds::MysqlInputType;
 
 int main() {
 
-    createDBTableBinds( HOST, USER, PASSWORD, "performance_schema", "include/_generated/performance_schemaBinds.h",
-                        "src/_generated/performance_schemaBinds.cpp", "_generated/performance_schemaBinds.h", 100 );
-    createDBTableBinds( HOST, USER, PASSWORD, "mybank", "include/_generated/myBankBinds.h",
-                        "src/_generated/myBankBinds.cpp", "_generated/myBankBinds.h", 100 );
-    createDBTableBinds( HOST, USER, PASSWORD, "giraffe", "include/_generated/giraffeBinds.h",
-                        "src/_generated/giraffeBinds.cpp", "_generated/giraffeBinds.h", 100 );
+    std::string salary( "salary" );
+    std::string_view flags( "flags" );
 
+    auto table = makeInputBindsArray( Bind<VARCHAR>( "name", 100 ), Bind<INT>( "id" ), Bind<TINYINT>( "age" ),
+                                      Bind<VARCHAR>( "location", 100 ), Bind<DECIMAL>( salary ),
+                                      Bind<BOOLEAN>( "married" ), Bind<BINARY>( flags, 100 ) );
+    table["name"]->set_value( "John Wayne" );
+    table["id"]->isNull = true;
+    table["age"]->set_value( 32 );
+    table["married"]->set_value( false );
+    table["location"]->set_value( "Brownsville, TX" );
+    table[salary]->set_value( 678.93 );
+    table[flags]->set_value( "0b10100010" );
+
+    table.displayFields();
     return 0;
 }
